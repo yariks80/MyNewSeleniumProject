9210628cb3f8e4e7e907fa02ab8f289246705360
iaroslav.sviatkin <iaroslav.sviatkin.globallogic.com>
2020-12-06 11:31:40 +0200

    commit1

--- src/test/java/com/it/project/MyTest.java
+++ src/test/java/com/it/project/MyTest.java
@@ -1,2 +1,21 @@
-package com.it.project;public class MyTest {
+package com.it.project;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.Test;
+
+public class MyTest {
+
+    @Test
+    public void test() throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.i.ua/");
+        Thread.sleep(2000);
+        driver.quit();
+    }
 }
